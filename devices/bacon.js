be500fda60189f948607db1668ad2c19407c00fe
Oğuzhan <unequaled86@gmail.com>
2020-07-06 19:32:27 +0300

    init

---
+++ devices/bacon.js
@@ -0,0 +1,13 @@
+{
+  "response": [
+    {
+        "datetime":1593090227,
+        "filename": "Xtended-XQ-v10.5-oneplus3-OFFICIAL-20200625.zip",
+        "id": "60fb5fd58a426171aa986e8351aae8bd",
+        "romtype": "OFFICIAL",
+        "size":662270857,
+        "url": "https://raw.githubusercontent.com/cCc-OS-Devices/devices/master/builds/bacon/Xtended-XQ-v10.5-bacon-OFFICIAL-20200625.zip",
+        "version": "XQ-v10.5"
+    }
+  ]
+}
